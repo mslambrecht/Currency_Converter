8133323ed74621786b6c1e96e212afbbee974528
mslambrecht <mslambrecht88@gmail.com>
2020-02-10 10:19:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View View) {
+
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+        String str = myTextField.getText().toString();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
